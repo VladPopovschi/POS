0f31960d3006afe39485019ebfc763d8f51903b5
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-15 18:58:20 +0300

    Add PointOfSaleDbContext.

---
+++ PointOfSale.Infrastructure/Persistence/DbContexts/PointOfSaleDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PointOfSale.Infrastructure.Persistence.DbContexts
+{
+    public class PointOfSaleDbContext : DbContext
+    {
+        public PointOfSaleDbContext(DbContextOptions<PointOfSaleDbContext> options) : base(options)
+        {
+        }
+    }
+}
