03eaf9e63a2358ac059f4b72c993a1444133049d
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-15 13:28:25 +0300

    Add Client Entity.

---
+++ PointOfSale.Domain/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PointOfSale.Domain.Entities
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTimeOffset TimestampCreated { get; set; }
+    }
+}
