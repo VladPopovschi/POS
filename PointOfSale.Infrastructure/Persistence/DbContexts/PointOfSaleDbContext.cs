ffb66403ed5a8036405019026fce9f01db7e8c3a
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-15 19:01:52 +0300

    Add DbSets to the PointOfSaleDbContext.

--- PointOfSale.Infrastructure/Persistence/DbContexts/PointOfSaleDbContext.cs
+++ PointOfSale.Infrastructure/Persistence/DbContexts/PointOfSaleDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PointOfSale.Domain.Entities;
 
 namespace PointOfSale.Infrastructure.Persistence.DbContexts
 {
@@ -7,5 +8,15 @@ namespace PointOfSale.Infrastructure.Persistence.DbContexts
         public PointOfSaleDbContext(DbContextOptions<PointOfSaleDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<SaleTransaction> SaleTransactions { get; set; }
+
+        public DbSet<SaleTransactionProduct> SaleTransactionProducts { get; set; }
     }
 }
