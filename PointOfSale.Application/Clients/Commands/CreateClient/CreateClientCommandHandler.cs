4c6cb8d454ff6eba17e07f96632c87034699abdf
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-20 10:55:23 +0300

    Add CreateClientCommandHandler.

---
+++ PointOfSale.Application/Clients/Commands/CreateClient/CreateClientCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace PointOfSale.Application.Clients.Commands.CreateClient
+{
+    public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, int>
+    {
+        public async Task<int> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
