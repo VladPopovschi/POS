ffae6a6f6e56c24105703215cd5d21410a8c57c7
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-20 10:48:39 +0300

    Add CreateClientCommand class.

---
+++ PointOfSale.Application/Clients/Commands/CreateClient/CreateClientCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Clients.Commands.CreateClient
+{
+    public class CreateClientCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+    }
+}
