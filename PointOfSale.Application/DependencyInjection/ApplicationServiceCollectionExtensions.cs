b8ac1d595f4a6cbcf12afcabb1442d2ef980d769
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-20 10:05:03 +0300

    Add MediatR package. Configure Dependency injection for the Application layer.

---
+++ PointOfSale.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PointOfSale.Application.DependencyInjection
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
