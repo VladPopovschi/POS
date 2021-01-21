21423d1e0fa4ccc1e05bbf7657b1c5d9f2056523
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-21 09:36:29 +0300

    Add FluentValidation.DependencyInjectionExtensions package. Add CreateClientCommandValidator.

---
+++ PointOfSale.Application/Clients/Commands/CreateClient/CreateClientCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Clients.Commands.CreateClient
+{
+    public class CreateClientCommandValidator : AbstractValidator<CreateClientCommand>
+    {
+        public CreateClientCommandValidator()
+        {
+            RuleFor(command => command.Name).NotEmpty();
+        }
+    }
+}
