98696875e2aa0c08cc47d1f806ad0763a982c72d
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-25 17:09:22 +0300

    Add UpdateClientCommandValidator.

---
+++ PointOfSale.Application/Clients/Commands/UpdateClient/UpdateClientCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Clients.Commands.UpdateClient
+{
+    public class UpdateClientCommandValidator : AbstractValidator<UpdateClientCommand>
+    {
+        public UpdateClientCommandValidator()
+        {
+            RuleFor(command => command.Id).GreaterThan(0);
+            RuleFor(command => command.Name).NotEmpty();
+        }
+    }
+}
