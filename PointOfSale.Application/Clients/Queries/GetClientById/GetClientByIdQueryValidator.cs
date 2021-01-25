d5789fdc008ad2a882e66b44f26e9b1a02d6ff5e
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-25 09:48:31 +0300

    Add GetClientByIdQueryValidator.

---
+++ PointOfSale.Application/Clients/Queries/GetClientById/GetClientByIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Clients.Queries.GetClientById
+{
+    public class GetClientByIdQueryValidator : AbstractValidator<GetClientByIdQuery>
+    {
+        public GetClientByIdQueryValidator()
+        {
+            RuleFor(query => query.Id).GreaterThan(0);
+        }
+    }
+}
