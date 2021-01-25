1f32f5229b481fe2fee99d59ebb243570e370382
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-25 09:44:40 +0300

    Add GetClientByIdQuery class. Add Client model.

---
+++ PointOfSale.Application/Clients/Queries/GetClientById/GetClientByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PointOfSale.Application.Models;
+
+namespace PointOfSale.Application.Clients.Queries.GetClientById
+{
+    public class GetClientByIdQuery : IRequest<Client>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PointOfSale.Application.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTimeOffset TimestampCreated { get; set; }
+    }
+}
