155c41f676b83d660e3d39ac5d0ceaa91e5edf81
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-25 09:51:27 +0300

    Add GetClientByIdQueryHandler.

---
+++ PointOfSale.Application/Clients/Queries/GetClientById/GetClientByIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using PointOfSale.Application.Models;
+
+namespace PointOfSale.Application.Clients.Queries.GetClientById
+{
+    public class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, Client>
+    {
+        public async Task<Client> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
