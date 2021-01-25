f57ece5fbd950b9ee43d0a4a2cd7abe51f03c5d2
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-25 17:07:56 +0300

    Add UpdateClientCommandHandler implementation.

---
+++ PointOfSale.Application/Clients/Commands/UpdateClient/UpdateClientCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Clients.Commands.UpdateClient
+{
+    public class UpdateClientCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Clients/Commands/UpdateClient/UpdateClientCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PointOfSale.Application.Exceptions;
+using PointOfSale.Application.Interfaces.DbContexts;
+using PointOfSale.Domain.Entities;
+
+namespace PointOfSale.Application.Clients.Commands.UpdateClient
+{
+    public class UpdateClientCommandHandler : AsyncRequestHandler<UpdateClientCommand>
+    {
+        private readonly IPointOfSaleContext _pointOfSaleContext;
+
+        public UpdateClientCommandHandler(IPointOfSaleContext pointOfSaleContext)
+        {
+            _pointOfSaleContext = pointOfSaleContext;
+        }
+
+        protected override async Task Handle(UpdateClientCommand command, CancellationToken cancellationToken)
+        {
+            var client = await _pointOfSaleContext.Clients
+                .SingleOrDefaultAsync(client => client.Id == command.Id, cancellationToken);
+
+            ValidateTheExistenceOfTheClient(command, client);
+            await ValidateTheUniquenessOfTheClientName(command, cancellationToken);
+
+            client.Name = command.Name;
+            await _pointOfSaleContext.SaveChangesAsync(cancellationToken);
+        }
+
+        private static void ValidateTheExistenceOfTheClient(UpdateClientCommand command, Client clientFromDatabase)
+        {
+            if (clientFromDatabase == null)
+            {
+                throw new NotFoundException($"The Client with Id {command.Id} not found in the database");
+            }
+        }
+
+        private async Task ValidateTheUniquenessOfTheClientName(UpdateClientCommand command, CancellationToken cancellationToken)
+        {
+            if (await _pointOfSaleContext
+                .Clients
+                .AnyAsync(client => client.Name.ToUpper() == command.Name.ToUpper(), cancellationToken))
+            {
+                throw new ValidationException($"The client with the name {command.Name} already exists in the database.");
+            }
+        }
+    }
+}
