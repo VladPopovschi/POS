5ab59dd39b27445af226ca9bc490c4d27003e315
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-26 09:15:55 +0300

    Add CreateStoreCommand and Validator.

---
+++ PointOfSale.Application/Stores/Commands/CreateStore/CreateStoreCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Stores.Commands.CreateStore
+{
+    public class CreateStoreCommand : IRequest<int>
+    {
+        public string GLN { get; set; }
+
+        public int ClientId { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Stores/Commands/CreateStore/CreateStoreCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Stores.Commands.CreateStore
+{
+    public class CreateStoreCommandValidator : AbstractValidator<CreateStoreCommand>
+    {
+        public CreateStoreCommandValidator()
+        {
+            RuleFor(command => command.GLN).NotEmpty();
+            RuleFor(command => command.ClientId).GreaterThan(0);
+        }
+    }
+}
