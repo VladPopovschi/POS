e05463103ddf9c61cc13ed1b6b2deaadc58db9e0
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-26 14:51:33 +0300

    Add StoreModel, GetStoreByIdQuery and validator.

---
+++ PointOfSale.Application/Models/StoreModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PointOfSale.Application.Models
+{
+    public class StoreModel
+    {
+        public int Id { get; set; }
+
+        public string GLN { get; set; }
+
+        public int ClientId { get; set; }
+
+        public DateTimeOffset TimestampCreated { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Stores/Queries/GetStoreById/GetStoreByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PointOfSale.Application.Models;
+
+namespace PointOfSale.Application.Stores.Queries.GetStoreById
+{
+    public class GetStoreByIdQuery : IRequest<StoreModel>
+    {
+        public int Id { get; set; }
+    }
+}
