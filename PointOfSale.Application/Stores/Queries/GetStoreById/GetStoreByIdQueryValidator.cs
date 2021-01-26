2deda2dfd1580bd49d2bd529c59fe6ad9bd0eb74
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-26 14:51:52 +0300

    Add GetStoreByIdQueryValidator

---
+++ PointOfSale.Application/Stores/Queries/GetStoreById/GetStoreByIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Stores.Queries.GetStoreById
+{
+    public class GetStoreByIdQueryValidator : AbstractValidator<GetStoreByIdQuery>
+    {
+        public GetStoreByIdQueryValidator()
+        {
+            RuleFor(query => query.Id).GreaterThan(0);
+        }
+    }
+}
