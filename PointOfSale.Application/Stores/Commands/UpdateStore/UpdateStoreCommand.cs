8faaf8174372b9c481119d690a5f5a91bd31a0e3
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-27 17:02:26 +0300

    Add UpdateStoreCommand and UpdateStoreCommandValidator.

---
+++ PointOfSale.Application/Stores/Commands/UpdateStore/UpdateStoreCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Stores.Commands.UpdateStore
+{
+    public class UpdateStoreCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string GLN { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Stores/Commands/UpdateStore/UpdateStoreCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Stores.Commands.UpdateStore
+{
+    public class UpdateStoreCommandValidator : AbstractValidator<UpdateStoreCommand>
+    {
+        public UpdateStoreCommandValidator()
+        {
+            RuleFor(command => command.Id).GreaterThan(0);
+            RuleFor(command => command.GLN).NotEmpty();
+        }
+    }
+}
