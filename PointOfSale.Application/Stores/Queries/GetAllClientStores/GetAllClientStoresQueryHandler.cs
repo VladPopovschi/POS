04d924d99a4b7aab095a7a91723b7f5ee205a479
Vlad Popovschi <vlad.popovschi@detego.com>
2021-01-27 16:19:38 +0300

    Add logic for the GetAllClientStoresQueryHandler.

--- PointOfSale.Application/Stores/Queries/GetAllClientStores/GetAllClientStoresQueryHandler.cs
+++ PointOfSale.Application/Stores/Queries/GetAllClientStores/GetAllClientStoresQueryHandler.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PointOfSale.Application.Exceptions;
 using PointOfSale.Application.Interfaces.DbContexts;
 using PointOfSale.Application.Models;
+using PointOfSale.Domain.Entities;
 
 namespace PointOfSale.Application.Stores.Queries.GetAllClientStores
 {
-    public class GetAllClientStoresQueryHandler : RequestHandler<GetAllClientStoresQuery, List<StoreModel>>
+    public class GetAllClientStoresQueryHandler : IRequestHandler<GetAllClientStoresQuery, List<StoreModel>>
     {
         private readonly IPointOfSaleContext _pointOfSaleContext;
 
@@ -14,8 +19,35 @@ namespace PointOfSale.Application.Stores.Queries.GetAllClientStores
             _pointOfSaleContext = pointOfSaleContext;
         }
 
-        protected override List<StoreModel> Handle(GetAllClientStoresQuery request)
+        public async Task<List<StoreModel>> Handle(GetAllClientStoresQuery query, CancellationToken cancellationToken)
         {
+            var client = await _pointOfSaleContext
+                .Clients
+                .AsNoTracking()
+                .Include(client => client.Stores)
+                .SingleOrDefaultAsync(client => client.Id == query.ClientId, cancellationToken);
+
+            ValidateTheExistenceOfTheClient(query, client);
+
+            var storeModels = new List<StoreModel>();
+
+            client.Stores.ForEach(store => storeModels.Add(new StoreModel
+            {
+                Id = store.Id,
+                GLN = store.GLN,
+                ClientId = store.ClientId,
+                TimestampCreated = store.TimestampCreated
+            }));
+
+            return storeModels;
+        }
+
+        private static void ValidateTheExistenceOfTheClient(GetAllClientStoresQuery query, Client clientFromDatabase)
+        {
+            if (clientFromDatabase == null)
+            {
+                throw new NotFoundException($"The Client with Id {query.ClientId} not found in the database");
+            }
         }
     }
 }
