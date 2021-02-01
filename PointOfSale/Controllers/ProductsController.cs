1a48a684c8e293402552862e9d47752f3cf35795
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-01 15:00:51 +0300

    Add Action Method for the CreateProductCommand.

---
+++ PointOfSale/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PointOfSale.Application.Products.Commands.CreateProduct;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace PointOfSale.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ProductsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        /// <summary>
+        /// Создание продукта
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(Status200OK)]
+        [ProducesResponseType(Status400BadRequest)]
+        [ProducesResponseType(Status404NotFound)]
+        public async Task<ActionResult<int>> Create(CreateProductCommand command)
+        {
+            var createdProductId = await _sender.Send(command);
+
+            return createdProductId;
+        }
+    }
+}
