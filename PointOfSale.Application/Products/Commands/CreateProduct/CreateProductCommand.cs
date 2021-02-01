56ebf13afc2833eaf3c8e5a7690b9b400ab3d07f
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-01 10:15:25 +0300

    Add CreateProductCommand.

---
+++ PointOfSale.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string GTIN { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public int ClientId { get; set; }
+    }
+}
