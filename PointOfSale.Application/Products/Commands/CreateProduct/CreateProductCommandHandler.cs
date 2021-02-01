2a664712511b6915f83c2f870c7809e7b9b286fd
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-01 10:27:06 +0300

    Add CreateProductCommandHandler.

---
+++ PointOfSale.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using PointOfSale.Application.Interfaces.DbContexts;
+
+namespace PointOfSale.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+    {
+        private readonly IPointOfSaleContext _pointOfSaleContext;
+
+        public CreateProductCommandHandler(IPointOfSaleContext pointOfSaleContext)
+        {
+            _pointOfSaleContext = pointOfSaleContext;
+        }
+
+        public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
