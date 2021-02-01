7dd125bce2957cd932028a86e75c9b62282c00b2
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-01 10:24:03 +0300

    Add CreateProductCommandValidator.

---
+++ PointOfSale.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(command => command.Name).NotEmpty();
+            RuleFor(command => command.GTIN).NotEmpty();
+            RuleFor(command => command.Price).GreaterThan(0);
+            RuleFor(command => command.ClientId).GreaterThan(0);
+        }
+    }
+}
