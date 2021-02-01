d4905bf8b8dfd2c441fe83b0d7c770f8efc506b7
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-01 14:52:54 +0300

    Add Client Validation for the CreateProductCommandHandler.

--- PointOfSale.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
+++ PointOfSale.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -19,6 +19,8 @@ namespace PointOfSale.Application.Products.Commands.CreateProduct
         public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             await ValidateTheUniquenessOfTheProductGTIN(command, cancellationToken);
+
+            await ValidateTheExistenceOfTheClient(command, cancellationToken);
         }
 
         private async Task ValidateTheUniquenessOfTheProductGTIN(CreateProductCommand command, CancellationToken cancellationToken)
@@ -28,6 +30,16 @@ namespace PointOfSale.Application.Products.Commands.CreateProduct
                 .AnyAsync(product => product.GTIN.ToUpper() == command.GTIN.ToUpper(), cancellationToken))
             {
                 throw new ValidationException($"A product with GTIN {command.GTIN} already exists in the database.");
+            }
+        }
+
+        private async Task ValidateTheExistenceOfTheClient(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            if (!await _pointOfSaleContext
+                .Clients
+                .AnyAsync(client => client.Id == command.ClientId, cancellationToken))
+            {
+                throw new NotFoundException($"The Client with Id {command.ClientId} not found in the database");
             }
         }
     }
