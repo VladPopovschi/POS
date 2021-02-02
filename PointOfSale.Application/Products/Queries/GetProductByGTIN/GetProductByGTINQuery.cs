7afcbafcff2d80e41d621f5dda61d73d71eadc18
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-02 13:50:28 +0300

    Add GetProductByGTINQuery.

---
+++ PointOfSale.Application/Products/Queries/GetProductByGTIN/GetProductByGTINQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PointOfSale.Application.Models;
+
+namespace PointOfSale.Application.Products.Queries.GetProductByGTIN
+{
+    public class GetProductByGTINQuery : IRequest<ProductModel>
+    {
+        public string GTIN { get; set; }
+    }
+}
