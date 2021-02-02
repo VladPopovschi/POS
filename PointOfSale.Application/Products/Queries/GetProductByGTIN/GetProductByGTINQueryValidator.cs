316d11e55b4aef38d7218d916cfadbe492ec4956
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-02 13:51:39 +0300

    Add GetProductByGTINQueryValidator.

---
+++ PointOfSale.Application/Products/Queries/GetProductByGTIN/GetProductByGTINQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Products.Queries.GetProductByGTIN
+{
+    public class GetProductByGTINQueryValidator : AbstractValidator<GetProductByGTINQuery>
+    {
+        public GetProductByGTINQueryValidator()
+        {
+            RuleFor(query => query.GTIN).NotEmpty();
+        }
+    }
+}
