a8bd78aef09e29aef91dc5ff91b2f7d47d913d49
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-04 12:55:36 +0300

    Add UpdateProductCommand and Validator.

---
+++ PointOfSale.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace PointOfSale.Application.Products.Commands.UpdateProduct
+{
+    public class UpdateProductCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string GTIN { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Products/Commands/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.Products.Commands.UpdateProduct
+{
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).GreaterThan(0);
+            RuleFor(command => command.Name).NotEmpty();
+            RuleFor(command => command.GTIN).NotEmpty();
+            RuleFor(command => command.Price).GreaterThan(0);
+        }
+    }
+}
