df3fa10c63680cefd3c07008b206d63174fc0ccc
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-08 19:43:42 +0300

    Add CreateSaleTransactionCommandHandler.

---
+++ PointOfSale.Application/SaleTransactions/Commands/CreateSaleTransaction/CreateSaleTransactionCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using PointOfSale.Application.Interfaces.DbContexts;
+
+namespace PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransaction
+{
+    public class CreateSaleTransactionCommandHandler : AsyncRequestHandler<CreateSaleTransactionCommand>
+    {
+        private readonly IPointOfSaleContext _pointOfSaleContext;
+
+        public CreateSaleTransactionCommandHandler(IPointOfSaleContext pointOfSaleContext)
+        {
+            _pointOfSaleContext = pointOfSaleContext;
+        }
+
+        protected override async Task Handle(CreateSaleTransactionCommand request, CancellationToken cancellationToken)
+        {
+            // Валидация существования магазина
+            // Валидация существования каждого продукта
+            // Валидация принадлежности каждого продукта клиента магазина
+            throw new System.NotImplementedException();
+        }
+    }
+}
