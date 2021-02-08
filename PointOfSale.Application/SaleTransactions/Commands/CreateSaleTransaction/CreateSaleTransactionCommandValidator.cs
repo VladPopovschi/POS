2cb6cab664a9cdd1c9debf31fff8765821f00c5a
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-08 19:39:58 +0300

    Add CreateSaleTransactionCommandValidator.

---
+++ PointOfSale.Application/SaleTransactions/Commands/CreateSaleTransaction/CreateSaleTransactionCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransaction
+{
+    public class CreateSaleTransactionCommandValidator : AbstractValidator<CreateSaleTransactionCommand>
+    {
+        public CreateSaleTransactionCommandValidator()
+        {
+            RuleFor(command => command.StoreId).GreaterThan(0);
+
+            RuleFor(command => command.SaleTransactionProducts)
+                .NotEmpty()
+                .ForEach(collection => collection.Must(product => product.Quantity > 0))
+                .ForEach(collection => collection.Must(product => product.ProductId > 0));
+        }
+    }
+}
