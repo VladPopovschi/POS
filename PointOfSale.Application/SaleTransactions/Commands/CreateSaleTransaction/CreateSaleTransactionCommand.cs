7e1469d50d2c3356565edd964c8b1e0546636745
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-08 19:31:00 +0300

    Add CreateSaleTransactionCommand.

---
+++ PointOfSale.Application/SaleTransactions/Commands/CreateSaleTransaction/CreateSaleTransactionCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransaction
+{
+    public class CreateSaleTransactionCommand : IRequest
+    {
+        public int StoreId { get; set; }
+
+        public List<SaleTransactionProductModel> SaleTransactionProducts { get; set; }
+    }
+
+    public class SaleTransactionProductModel
+    {
+        public decimal Quantity { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
