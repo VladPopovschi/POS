e38ed21bd2a2df36ea6fa63f5a086c901628ffc0
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-09 11:46:36 +0300

    Add Create action method.

---
+++ PointOfSale/Controllers/SaleTransactionsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransaction;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace PointOfSale.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SaleTransactionsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public SaleTransactionsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        /// <summary>
+        /// Создание транзакции продажи
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(Status204NoContent)]
+        [ProducesResponseType(Status400BadRequest)]
+        [ProducesResponseType(Status404NotFound)]
+        public async Task<IActionResult> Create(CreateSaleTransactionCommand command)
+        {
+            await _sender.Send(command);
+
+            return new NoContentResult();
+        }
+    }
+}
