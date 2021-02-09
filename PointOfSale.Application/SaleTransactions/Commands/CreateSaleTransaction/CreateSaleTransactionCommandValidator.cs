3596f8f8a6ae772d5d23676afad0c99801ca3f1d
Vlad Popovschi <vlad.popovschi@detego.com>
2021-02-09 11:59:57 +0300

    Fix validation of the CreateSaleTransactionCommandValidator.

--- PointOfSale.Application/SaleTransactions/Commands/CreateSaleTransaction/CreateSaleTransactionCommandValidator.cs
+++ PointOfSale.Application/SaleTransactions/Commands/CreateSaleTransaction/CreateSaleTransactionCommandValidator.cs
@@ -9,9 +9,19 @@ namespace PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransactio
             RuleFor(command => command.StoreId).GreaterThan(0);
 
             RuleFor(command => command.SaleTransactionProducts)
-                .NotEmpty()
-                .ForEach(collection => collection.Must(product => product.Quantity > 0))
-                .ForEach(collection => collection.Must(product => product.ProductId > 0));
+                .NotEmpty();
+
+            RuleFor(command => command.SaleTransactionProducts)
+                .ForEach(collection => collection.Must(product => product != null)
+                .WithMessage($"Every {nameof(SaleTransactionProductModel)} object passed must not be null"));
+
+            RuleFor(command => command.SaleTransactionProducts)
+                .ForEach(collection => collection.Must(product => product.Quantity > 0)
+                .WithMessage("The quantity of each product sold must be greater than 0."));
+
+            RuleFor(command => command.SaleTransactionProducts)
+                .ForEach(collection => collection.Must(product => product.ProductId > 0)
+                .WithMessage("The identifier of each product sold must be greater than 0."));
         }
     }
 }
