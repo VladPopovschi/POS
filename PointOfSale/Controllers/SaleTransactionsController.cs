dc500fcc22c7f4dfb688fb60e4c7cc701d1a0f62
Vlad Popovschi <vlad.popovschi@detego.com>
2021-03-01 14:46:10 +0300

    Add GetAllStoreSalesTransactions action method.

--- PointOfSale/Controllers/SaleTransactionsController.cs
+++ PointOfSale/Controllers/SaleTransactionsController.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using PointOfSale.Application.Models;
 using PointOfSale.Application.SaleTransactions.Commands.CreateSaleTransaction;
+using PointOfSale.Application.SaleTransactions.Queries.GetAllStoreSalesTransactions;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace PointOfSale.Controllers
@@ -29,6 +32,23 @@ namespace PointOfSale.Controllers
             await _sender.Send(command);
 
             return new NoContentResult();
+        }
+
+        /// <summary>
+        /// Получение всех транзакций продажи магазина
+        /// </summary>
+        [HttpGet("AllStoreSalesTransactions/{storeId}")]
+        [ProducesResponseType(Status200OK)]
+        [ProducesResponseType(Status400BadRequest)]
+        [ProducesResponseType(Status404NotFound)]
+        public async Task<ActionResult<List<SaleTransactionModel>>> GetAllStoreSalesTransactions(int storeId)
+        {
+            var storeSaleTransactions = await _sender.Send(new GetAllStoreSalesTransactionsQuery
+            {
+                StoreId = storeId
+            });
+
+            return storeSaleTransactions;
         }
     }
 }
