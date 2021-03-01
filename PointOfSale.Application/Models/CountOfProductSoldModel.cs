6159f905aa21660342f51d1a13d40541072aee25
Vlad Popovschi <vlad.popovschi@detego.com>
2021-03-01 15:25:44 +0300

    Add GetQuantityOfClientProductsSoldQuery and model.

---
+++ PointOfSale.Application/Models/CountOfProductSoldModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PointOfSale.Application.Models
+{
+    public class QuantityOfProductSoldModel
+    {
+        public string ProductGTIN { get; set; }
+
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ PointOfSale.Application/Products/Queries/GetQuantityOfClientProductsSold/GetQuantityOfClientProductsSoldQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using PointOfSale.Application.Models;
+
+namespace PointOfSale.Application.Products.Queries.GetQuantityOfClientProductsSold
+{
+    public class GetQuantityOfClientProductsSoldQuery : IRequest<List<QuantityOfProductSoldModel>>
+    {
+        public int ClientId { get; set; }
+    }
+}
