faf3cdeccf8345a3b1795d5f5c81383a6e41c81f
Vlad Popovschi <vlad.popovschi@detego.com>
2021-03-01 14:32:43 +0300

    Add GetAllStoreSalesTransactionsQueryValidator.

---
+++ PointOfSale.Application/SaleTransactions/Queries/GetAllStoreSalesTransactions/GetAllStoreSalesTransactionsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PointOfSale.Application.SaleTransactions.Queries.GetAllStoreSalesTransactions
+{
+    public class GetAllStoreSalesTransactionsQueryValidator : AbstractValidator<GetAllStoreSalesTransactionsQuery>
+    {
+        public GetAllStoreSalesTransactionsQueryValidator()
+        {
+            RuleFor(query => query.StoreId).GreaterThan(0);
+        }
+    }
+}
